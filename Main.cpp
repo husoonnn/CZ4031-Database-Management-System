ad77397f5da6b0a43084e324c21f07c2df195534
wenxiuuuuu <66111418+wenxiuuuuu@users.noreply.github.com>
2021-09-22 15:19:49 +0800

    mem pool

--- Main.cpp
+++ Main.cpp
@@ -5,3 +5,11 @@
 #include <vector>
 #include <tuple>
 #include <unordered_map>
+
+using namespace std;
+
+int main(){
+    cout << "hello world" <<endl;
+    ifstream dataFile("/Users/wenxiu/Documents/GitHub/CZ4031-Database-System-Principles/data/data.tsv");
+    cout << "bye" <<endl; 
+}
