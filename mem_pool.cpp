102faa0a730a1904d29895ed85be93881b154527
jumboy <lim.jinyang@dhs.sg>
2021-09-29 19:50:03 +0800

    mempool updates

--- mem_pool.cpp
+++ mem_pool.cpp
@@ -8,21 +8,29 @@
 
 using namespace std;
 
+//Constructor 
 MemoryPool::MemoryPool(std::size_t MaxPoolSize, std::size_t BlockSize) {
   
   maxpoolsize = MaxPoolSize;
-  poolsizeleft = maxpoolsize - (num_of_blocks*blocksize);
+  poolsizeleft = maxpoolsize - (numofblocks*blocksize);
   blocksize = BlockSize;
   blocksizeused = 0;
   blocksizeleft = 0;
+  numofblocks = 0; 
 
+  //pointers 
+  //allocating pool ptr to maxpoolsize bytes of storage 
   pool = operator new(maxpoolsize);
-
+  //initialize pool all to null
+  std::memset(pool, '\0', maxpoolsize);
+  //set block ptr to null ptr  
+  block = nullptr; 
 }
 
-bool MemoryPool::checkblocksize(std::size_t recordsize, std::size_t BlockSizeLeft){
-
-  if (recordsize > BlockSizeLeft){
+//done. 
+//check whether the block can insert the record 
+bool MemoryPool::CheckBlockSize(std::size_t recordsize){
+  if (recordsize > blocksizeleft){
     std::cout << "Not enough memory to insert record into block" << endl;
     return false;
   }
@@ -31,17 +39,98 @@ bool MemoryPool::checkblocksize(std::size_t recordsize, std::size_t BlockSizeLef
   }
 }
 
-Address MemoryPool::Allocateblock(bool CheckBlockSize){
+// insert record into block 
+Address MemoryPool::AllocateBlock(std:: size_t recordsize){
+  //if block size cannot fit new record, create new block
   if (!CheckBlockSize){
-    //allocate new block
-    //from pool get new block get new address
+    //create a new block
+    NewBlock();
+    //if new block cannot be created, exception error 
+    if (!NewBlock()){
+      throw std::logic_error("Failed to allocate new block!");
+    };
+  }; 
+
+  //insert into the block 
+  //update variables
+  short int offset = blocksizeused; 
+
+  blocksizeused += recordsize;
+  blocksizeleft = blocksize - recordsize;
+  //update pointer after insert 
+  block = (char *)block + recordsize;
+
+  // Return the new memory space to put in the record.
+  Address recordAddress = {block, offset};
+  return recordAddress;
+}
+
+//done. 
+//check whether the maxpoolsize is exceeded; if exceeded cannot create new block 
+bool MemoryPool::CheckMaxPool(){
+  if (blocksize + blocksizeused < maxpoolsize){
+    return false; 
   }
-  else{
-    //add to current block
-    //return current block new address
+  else {
+    return true; 
   }
 }
 
+//done. 
+bool MemoryPool::NewBlock(){
+  //create newblock if we didn't exceed maxpoolsize 
+  if (!CheckMaxPool){
+    block = (char *)pool + numofblocks * blocksize; //move pointer to start of new block 
+    blocksizeused += blocksize; //update blocksizeused 
+    blocksizeused = 0; //reset block size used 
+    numofblocks += 1; 
+
+    std::cout << "New block successfully created!" << endl; 
+    return true; 
+  }
+  else {
+    std::cout << "Max Pool Size exceeded! Unable to add more blocks" << endl; 
+    return false; 
+  }
+}
+
+//copied from repo. 
+// Give a block address, offset and size, returns the data there.
+void* MemoryPool::loadFromDisk(Address address, std::size_t size)
+{
+  void* mainMemoryAddress = operator new(size);
+  std::memcpy(mainMemoryAddress, (char *)address.blockAddress + address.offset, size);
+
+  // Update blocks accessed
+  numofblocks++;
+
+  return mainMemoryAddress;
+}
+
+// Saves something into the disk. Returns disk address.
+Address MemoryPool::saveToDisk(void *itemAddress, std::size_t size)
+{
+  Address diskAddress = AllocateBlock(size);
+  std::memcpy((char *)diskAddress.blockAddress + diskAddress.offset, itemAddress, size);
+
+  // Update blocks accessed
+  numofblocks++;
+
+  return diskAddress;
+}
+
+// Update data in disk if I have already saved it before.
+Address MemoryPool::saveToDisk(void *itemAddress, std::size_t size, Address diskAddress)
+{
+  std::memcpy((char *)diskAddress.blockAddress + diskAddress.offset, itemAddress, size);
+
+  // Update blocks accessed
+  numofblocks++;
+
+  return diskAddress;
+}
+
+//kiv maybe don;t do deallocation 
 Address MemoryPool::Deallocate(){
 
 }
