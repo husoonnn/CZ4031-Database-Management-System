ebf4acc944c5b252a598f023be812f74d5fac8a8
jumboy <lim.jinyang@dhs.sg>
2021-10-03 00:34:00 +0800

    search update and testing

--- Main.cpp
+++ Main.cpp
@@ -216,7 +216,7 @@ int main(){
 
     std::cout <<"*************************************Experiment 3*************************************"<<endl;
     // std::cout <<"Retrieving the attribute tconst of those movies with numVotes equal to 500..."<<endl;     
-    tree.search(tree.getRoot(), 120, 120);
+    tree.search(tree.getRoot(), 16, 16);
     tree.LLdisplay(tree.getRoot());
     // std::cout << endl;
     // std::cout <<"Number of index blocks the process accesses: "<<index.resetBlocksAccessed()<<endl; 
